74f980299ed927c5cd2bafaaf0ff0774e5f1bf42
Dzaki Mahadika <dz4k1m4h4d1k4@gmail.com>
2022-03-26 14:05:41 +0700

    createTeam yg beneran

--- app/pages/team/create.tsx
+++ app/pages/team/create.tsx
@@ -1,21 +1,22 @@
-import { Button, Input, Link, Select, Text, Textarea } from "@chakra-ui/react";
+import { Button, Icon, Input, Link, Select, Text, Textarea } from "@chakra-ui/react";
 import React from "react";
 import NextLink from "next/link";
+import { AttachmentIcon, ChevronLeftIcon } from "@chakra-ui/icons";
 
 const CreateTeamPage = () => {
   return (
     <div>
-      <div className="flex p-6">
-        <div>Back Button</div>
+      <div className="flex p-6 gap-6 items-center">
+        <div><ChevronLeftIcon fontSize={40} fontWeight="900" color="main.500"></ChevronLeftIcon></div>
         <div>
-          <Text fontSize={36} fontWeight="900" color="main.500">
+          <Text fontSize={24} fontWeight="900" color="main.500">
             Add Team
           </Text></div>
       </div>
       <div className="px-6 mt-[2.5rem] flex flex-col items-between">
         <div>
           <div className="mb-6">
-            <Text mb="8px">Leader Name</Text>
+            <Text mb="8px" fontWeight="500">Leader Name</Text>
             <Input focusBorderColor="main.500" size="md" type="text" />
           </div>
           <div className="mb-6">
@@ -29,9 +30,9 @@ const CreateTeamPage = () => {
           <div className="mb-6">
             <Text mb="8px">Institution/University</Text>
             <Select focusBorderColor="main.500" size="md" boxShadow="0 0 0 1px #D3D3D3" placeholder='Select your Institution'>
-              <option value='option1'>Option 1</option>
-              <option value='option2'>Option 2</option>
-              <option value='option3'>Option 3</option>
+              <option value='option1'>Telkom University</option>
+              <option value='option2'>Indonesia University</option>
+              <option value='option3'>Diponegoro University</option>
             </Select>
           </div>
           <div className="mb-6">
@@ -40,13 +41,30 @@ const CreateTeamPage = () => {
           </div>
           <div className="mb-6">
             <Text mb="8px">Add Attachment</Text>
-            <Input focusBorderColor="main.500" size="md" type="text" />
+            <Button
+              colorScheme="white"
+              color="gray.500"
+              border="2px solid #D3D3D3"
+              isFullWidth
+              fontWeight={400}
+              mb={4}
+              size="lg"
+              fontSize="md"
+              borderRadius={8}
+            >
+              <div className="flex gap-4 items-center">
+              <div><AttachmentIcon></AttachmentIcon></div>
+              <div>Upload File</div>
+              </div>
+            </Button>
           </div>
         </div>
       </div>
       <div className="mt-4 px-6">
         <Button
-          colorScheme="main"
+          colorScheme="white"
+          color="main.500"
+          border="2px solid #FF3DE0"
           isFullWidth
           fontWeight={400}
           mb={4}
