c1725f6a4473978ee5af030ed74df9e1c76791b0
Muhammad Ilham Mubarak <m.ilham.mubarak@gmail.com>
2022-03-26 14:13:05 +0700

    add team page

--- app/pages/team.tsx
+++ app/pages/team.tsx
@@ -3,6 +3,8 @@ import {
   InputGroup,
   InputLeftElement,
   InputRightElement,
+  Select,
+  Text,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 import DefaultLayout from "components/Layouts/DefaultLayout";
@@ -15,11 +17,60 @@ const Team = () => {
           Competition Team
         </h1>
         <InputGroup>
-          <Input />
+          <Input placeholder="Search for Competition" />
           <InputRightElement>
             <SearchIcon />
           </InputRightElement>
         </InputGroup>
+        <div className="mt-5 flex gap-5">
+          <div className="w-full">
+            <Text>Competition</Text>
+            <Select placeholder="Select option">
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Select>
+          </div>
+          <div className="w-full">
+            <Text>University</Text>
+            <Select placeholder="Select option">
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+              <option value="option3">Option 3</option>
+            </Select>
+          </div>
+        </div>
+
+        <div className="mt-[4rem] flex flex-col gap-4">
+          <div className="flex justify-center">
+            <div className="h-[140px] w-[420px] rounded-lg	bg-[green] flex items-end">
+              <div className="p-4 w-full">
+                <div className="flex w-full justify-between">
+                  <h1 className="text-white text-2xl font-black">SSATeam</h1>
+                  <p className="text-white">Designer, Programmer</p>
+                </div>
+                <div className="flex w-full justify-between">
+                  <h6 className="text-white">2 / 3 Members</h6>
+                  <p className="text-white">Smart IT Fest - UI/UX Design</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <div className="h-[140px] w-[420px] rounded-lg	bg-[green] flex items-end">
+              <div className="p-4 w-full">
+                <div className="flex w-full justify-between">
+                  <h1 className="text-white text-2xl font-black">SSATeam</h1>
+                  <p className="text-white">Designer, Programmer</p>
+                </div>
+                <div className="flex w-full justify-between">
+                  <h6 className="text-white">2 / 3 Members</h6>
+                  <p className="text-white">Smart IT Fest - UI/UX Design</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </DefaultLayout>
   );
