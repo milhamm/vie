bebdda1f3ef47cb45b10c78e456521369e199761
ridhodori <ridhodori48@gmail.com>
2022-04-09 13:17:18 +0700

    mbuh

--- app/pages/api/user/register.ts
+++ app/pages/api/user/register.ts
@@ -5,13 +5,23 @@ type Data = {
   name: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   switch (req.method) {
     case "POST":
-      // Register
+      const data = req.body;
+
+      
+      try {
+        const salt = await bcrypt.genSalt(10);
+        data.password = await bcrypt.hash(data.password, salt);
+        const user  = await prisma.user.create({ data: { ...data } });
+        res.send(user);
+      } catch (error) {
+        errorHandler(error, req, res);
+      }
       break;
     default:
       break;
