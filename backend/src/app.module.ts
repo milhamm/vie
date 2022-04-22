6af3fa39dd667f16b33f6ca1e67e758a668bc407
ridhodori <ridhodori48@gmail.com>
2022-04-22 23:24:34 +0700

    users login register

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TeamModule } from './team/team.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TeamModule],
+  imports: [TeamModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, Users } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+  async register(data: Prisma.UsersCreateInput) {
+    return this.prisma.users.create({ data });
+  }
+
+  async login(params: {
+    where?: Prisma.UsersWhereInput;
+    select?: Prisma.UsersSelect;
+  }): Promise<any> {
+    const { where, select } = params;
+    return this.prisma.users.findFirst({ where, select });
+  }
+}
