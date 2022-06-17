ae2e842823e41ff30043aa37dce0a2f098a5eac8
Dharaka Pranamya Mahadhika Damar <dharaka45@gmail.com>
2022-06-17 16:57:00 +0700

    update edit profile page

--- app/pages/profile/edit.tsx
+++ app/pages/profile/edit.tsx
@@ -1,5 +1,20 @@
+import {
+  Box,
+  Tabs,
+  TabList,
+  FormControl,
+  FormLabel,
+  Input,
+  Select,
+  Button,
+  TabPanels,
+  TabPanel,
+} from "@chakra-ui/react";
+
 import React from "react";
 import withAuth from "lib/withAuth";
+import { Image } from "@chakra-ui/react";
+import CommonTab from "components/CommonTab";
 
 export const getServerSideProps = withAuth(async (ctx) => {
   const token = ctx.req.cookies.token;
@@ -12,9 +27,83 @@ export const getServerSideProps = withAuth(async (ctx) => {
 
 const EditProfile = ({ token }) => {
   return (
-    <div>
-      <h1>Edit Profile</h1>
-    </div>
+    <>
+      <div className="p-8">
+        <h1 className="text-3xl font-black text-[#FF3DE0]">Profile</h1>
+      </div>
+      <div className="flex justify-center">
+        <Box boxSize="100px">
+          <Image src="https://bit.ly/dan-abramov" borderRadius="10px"></Image>
+        </Box>
+      </div>
+      <div className="p-8">
+        <Tabs colorScheme="pink" isFitted>
+          <TabList border="none">
+            <CommonTab>Personal Data</CommonTab>
+            <CommonTab>Skill</CommonTab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <div className="pt-4">
+                <FormControl>
+                  <div className="pb-4">
+                    <FormLabel htmlFor="full-name">Full Name</FormLabel>
+                    <Input id="full-name" placeholder="Muhammad Memeng" />
+                  </div>
+                  <div className="pb-4">
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input id="email" placeholder="memeng@gmail.com" />
+                  </div>
+                  <div className="pb-4">
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input id="password" placeholder="********" />
+                  </div>
+                  <div>
+                    <FormLabel htmlFor="university">University</FormLabel>
+                    <Select placeholder="Telkom University">
+                      <option value="option2">
+                        Institut Teknologi Bojongsoang
+                      </option>
+                      <option value="option1">Universitas Padjajaran</option>
+                    </Select>
+                  </div>
+                  <div className="pt-8">
+                    <Button isFullWidth bg="main.500" color="white">
+                      Update Profile
+                    </Button>
+                  </div>
+                </FormControl>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="pt-4">
+                <h1 className="font-bold">Your Skill</h1>
+                <div className="grid grid-cols-1 divide-y">
+                  <h1 className="pt-4 pb-4">UI/UX Designer</h1>
+                  <h1 className="pt-4 pb-4">Frontend Developer</h1>
+                  <h1 className="pt-4 pb-4">Backend Developer</h1>
+                  <h1 className="pt-4 pb-4">Data Science</h1>
+                </div>
+                <div className="pt-4">
+                  <h1 className="font-bold">Add Skill</h1>
+                  <div className="pt-4">
+                    <Input
+                      id="add-skill"
+                      placeholder="Skill (co: Fullstack Web Developer)"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="pt-8">
+                <Button isFullWidth bg="main.500" color="white">
+                  Update Profile
+                </Button>
+              </div>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
