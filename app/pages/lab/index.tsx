dd4e2bf29a6ebe0405f98f583f831ea1127746d2
Dharaka Pranamya Mahadhika Damar <dharaka45@gmail.com>
2022-06-18 20:58:52 +0700

    update lab index

--- app/pages/lab/index.tsx
+++ app/pages/lab/index.tsx
@@ -1,8 +1,50 @@
+import { Box, Button } from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
+
 const Lab = () => {
   return (
-    <div>
-      <h1>Lab</h1>
-    </div>
+    <>
+      <div className="p-8">
+        <h1 className="text-3xl font-black text-[#FF3DE0]">
+          RPLGBT Laboratory
+        </h1>
+        <div className="pt-8 flex justify-between">
+          <h1>Member</h1>
+          <div>
+            <Button size="sm" color="white" bg="main.500">
+              + Add
+            </Button>
+          </div>
+        </div>
+        <div className="pt-8 flex items-center">
+          <Box boxSize="60px">
+            <Image
+              src="https://bit.ly/dan-abramov"
+              borderRadius="100px"
+            ></Image>
+          </Box>
+          <h1 className="px-8">Muhammad Memeng</h1>
+        </div>
+        <div className="pt-8 flex items-center">
+          <Box boxSize="60px">
+            <Image
+              src="https://bit.ly/dan-abramov"
+              borderRadius="100px"
+            ></Image>
+          </Box>
+          <h1 className="px-8">Ridho Nobelion Sabililah Damar</h1>
+        </div>
+        <div className="pt-8 flex items-center">
+          <Box boxSize="60px">
+            <Image
+              src="https://bit.ly/dan-abramov"
+              borderRadius="100px"
+            ></Image>
+          </Box>
+          <h1 className="px-8">Jeki Mahadika Gunarto</h1>
+        </div>
+      </div>
+    </>
   );
 };
 
